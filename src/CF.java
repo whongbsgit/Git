a148bd3f109ed4868d5c757d81f694a3eb27e4c7
OooeOooahah <Mr_Xiao_Wu@outlook.com>
2019-11-29 16:48:27 +0800

    ss

---
+++ src/CF.java
@@ -0,0 +1,9 @@
+public class CF {
+    public static void main(String[] args) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.println(i+j+i*j);
+            }
+        }
+    }
+}
