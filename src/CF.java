d578613068813066ef64a3c8b4057c0183bbe750
OooeOooahah <Mr_Xiao_Wu@outlook.com>
2019-11-29 17:49:58 +0800

    modify i

--- src/CF.java
+++ src/CF.java
@@ -3,6 +3,7 @@ public class CF {
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j <= i; j++) {
                 System.out.println(i+j+i*j);
+                System.out.println(i);
             }
         }
     }
